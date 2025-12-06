be74ee4a246b584726be6cde15d0a91b3ed10927
Tên của bạn <trongdoan7689@gmail.com>
2025-12-06 10:06:09 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace khachsan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
