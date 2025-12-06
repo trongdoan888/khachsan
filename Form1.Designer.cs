80f01b357d5e1aafce15550509662b77fbb02feb
Tên của bạn <trongdoan7689@gmail.com>
2025-12-06 11:38:24 +0700

    trọng push

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,29 @@
         private void InitializeComponent()
         {
             splitContainer1 = new SplitContainer();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
             splitContainer2 = new SplitContainer();
+            button6 = new Button();
+            button5 = new Button();
+            button12 = new Button();
+            button11 = new Button();
+            button10 = new Button();
+            button9 = new Button();
+            button8 = new Button();
+            button7 = new Button();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            toolStripComboBox1 = new ToolStripComboBox();
+            dateTimePicker1 = new DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
             splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
             splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)splitContainer2).BeginInit();
+            splitContainer2.Panel1.SuspendLayout();
+            splitContainer2.Panel2.SuspendLayout();
             splitContainer2.SuspendLayout();
             SuspendLayout();
             // 
@@ -42,35 +60,192 @@
             splitContainer1.Dock = DockStyle.Fill;
             splitContainer1.Location = new Point(0, 0);
             splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
             // 
             // splitContainer1.Panel1
             // 
-            splitContainer1.Panel1.Controls.Add(splitContainer2);
-            splitContainer1.Size = new Size(1904, 1041);
-            splitContainer1.SplitterDistance = 651;
+            splitContainer1.Panel1.Controls.Add(button4);
+            splitContainer1.Panel1.Controls.Add(button3);
+            splitContainer1.Panel1.Controls.Add(button2);
+            splitContainer1.Panel1.Controls.Add(button1);
+            splitContainer1.Panel1.Paint += splitContainer1_Panel1_Paint_1;
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(splitContainer2);
+            splitContainer1.Panel2.Paint += splitContainer1_Panel2_Paint_1;
+            splitContainer1.Size = new Size(810, 589);
+            splitContainer1.SplitterDistance = 52;
             splitContainer1.TabIndex = 0;
+            splitContainer1.SplitterMoved += splitContainer1_SplitterMoved;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(367, 17);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 3;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(258, 17);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(149, 17);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(24, 17);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // splitContainer2
             // 
             splitContainer2.Dock = DockStyle.Fill;
             splitContainer2.Location = new Point(0, 0);
             splitContainer2.Name = "splitContainer2";
-            splitContainer2.Size = new Size(651, 1041);
-            splitContainer2.SplitterDistance = 217;
+            splitContainer2.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            splitContainer2.Panel1.Controls.Add(dateTimePicker1);
+            splitContainer2.Panel1.Controls.Add(button6);
+            splitContainer2.Panel1.Controls.Add(button5);
+            // 
+            // splitContainer2.Panel2
+            // 
+            splitContainer2.Panel2.Controls.Add(button12);
+            splitContainer2.Panel2.Controls.Add(button11);
+            splitContainer2.Panel2.Controls.Add(button10);
+            splitContainer2.Panel2.Controls.Add(button9);
+            splitContainer2.Panel2.Controls.Add(button8);
+            splitContainer2.Panel2.Controls.Add(button7);
+            splitContainer2.Size = new Size(810, 533);
+            splitContainer2.SplitterDistance = 93;
             splitContainer2.TabIndex = 0;
+            splitContainer2.SplitterMoved += splitContainer2_SplitterMoved;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(311, 36);
+            button6.Name = "button6";
+            button6.Size = new Size(36, 23);
+            button6.TabIndex = 1;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(24, 36);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 0;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(539, 247);
+            button12.Name = "button12";
+            button12.Size = new Size(266, 152);
+            button12.TabIndex = 5;
+            button12.Text = "button12";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(275, 247);
+            button11.Name = "button11";
+            button11.Size = new Size(266, 152);
+            button11.TabIndex = 4;
+            button11.Text = "button11";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(12, 247);
+            button10.Name = "button10";
+            button10.Size = new Size(266, 152);
+            button10.TabIndex = 3;
+            button10.Text = "button10";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(539, 44);
+            button9.Name = "button9";
+            button9.Size = new Size(266, 152);
+            button9.TabIndex = 2;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(275, 44);
+            button8.Name = "button8";
+            button8.Size = new Size(266, 152);
+            button8.TabIndex = 1;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(12, 44);
+            button7.Name = "button7";
+            button7.Size = new Size(266, 152);
+            button7.TabIndex = 0;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(32, 19);
+            // 
+            // toolStripComboBox1
+            // 
+            toolStripComboBox1.Name = "toolStripComboBox1";
+            toolStripComboBox1.Size = new Size(121, 23);
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(105, 36);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1904, 1041);
+            BackColor = SystemColors.InactiveCaption;
+            ClientSize = new Size(810, 589);
             Controls.Add(splitContainer1);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
             splitContainer1.ResumeLayout(false);
+            splitContainer2.Panel1.ResumeLayout(false);
+            splitContainer2.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)splitContainer2).EndInit();
             splitContainer2.ResumeLayout(false);
             ResumeLayout(false);
@@ -80,5 +255,20 @@
 
         private SplitContainer splitContainer1;
         private SplitContainer splitContainer2;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Button button6;
+        private Button button5;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripComboBox toolStripComboBox1;
+        private Button button7;
+        private Button button12;
+        private Button button11;
+        private Button button10;
+        private Button button9;
+        private Button button8;
+        private DateTimePicker dateTimePicker1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,25 @@ namespace khachsan
             InitializeComponent();
         }
 
-        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        private void splitContainer1_Panel2_Paint_1(object sender, PaintEventArgs e)
+        {
+            //
+        }
+
+        private void splitContainer1_Panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+            //
+        }
+
+        private void splitContainer2_SplitterMoved(object sender, SplitterEventArgs e)
         {
 
         }
 
-        private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
         {
 
         }
+
     }
 }
