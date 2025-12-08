bbc5d01ad5f864f4e83f7cfd3c8ea33787780257
4TH JULY <nguyentrongthang938@gmail.com>
2025-12-08 15:37:20 +0700

    đã thêm icon và khóa form của #

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -46,10 +46,9 @@
             lbllogin.BackColor = Color.Transparent;
             lbllogin.Font = new Font("Times New Roman", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lbllogin.ForeColor = Color.LemonChiffon;
-            lbllogin.Location = new Point(328, 139);
-            lbllogin.Margin = new Padding(2, 0, 2, 0);
+            lbllogin.Location = new Point(469, 232);
             lbllogin.Name = "lbllogin";
-            lbllogin.Size = new Size(566, 55);
+            lbllogin.Size = new Size(849, 81);
             lbllogin.TabIndex = 1;
             lbllogin.Text = "HOTEL MANAGEMENT";
             lbllogin.TextAlign = ContentAlignment.TopCenter;
@@ -60,10 +59,9 @@
             lblname.BackColor = Color.Transparent;
             lblname.Font = new Font("Times New Roman", 48F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             lblname.ForeColor = Color.Khaki;
-            lblname.Location = new Point(169, 50);
-            lblname.Margin = new Padding(2, 0, 2, 0);
+            lblname.Location = new Point(241, 83);
             lblname.Name = "lblname";
-            lblname.Size = new Size(873, 72);
+            lblname.Size = new Size(1306, 108);
             lblname.TabIndex = 2;
             lblname.Text = "PARKSIDE SUNLINE HOTEL";
             // 
@@ -73,10 +71,9 @@
             lblaim.BackColor = Color.Transparent;
             lblaim.Font = new Font("Times New Roman", 28F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblaim.ForeColor = Color.Wheat;
-            lblaim.Location = new Point(394, 210);
-            lblaim.Margin = new Padding(2, 0, 2, 0);
+            lblaim.Location = new Point(563, 350);
             lblaim.Name = "lblaim";
-            lblaim.Size = new Size(445, 43);
+            lblaim.Size = new Size(657, 63);
             lblaim.TabIndex = 3;
             lblaim.Text = "Front Office Management";
             // 
@@ -86,12 +83,12 @@
             lblUsername.BackColor = Color.Transparent;
             lblUsername.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblUsername.ForeColor = Color.Khaki;
-            lblUsername.Location = new Point(364, 512);
-            lblUsername.Margin = new Padding(2, 0, 2, 0);
+            lblUsername.Location = new Point(520, 817);
             lblUsername.Name = "lblUsername";
-            lblUsername.Size = new Size(154, 26);
+            lblUsername.Size = new Size(233, 41);
             lblUsername.TabIndex = 4;
             lblUsername.Text = "USERNAME ";
+            lblUsername.Click += lblUsername_Click;
             // 
             // lblPassword
             // 
@@ -99,10 +96,9 @@
             lblPassword.BackColor = Color.Transparent;
             lblPassword.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblPassword.ForeColor = Color.Khaki;
-            lblPassword.Location = new Point(364, 558);
-            lblPassword.Margin = new Padding(2, 0, 2, 0);
+            lblPassword.Location = new Point(520, 894);
             lblPassword.Name = "lblPassword";
-            lblPassword.Size = new Size(150, 26);
+            lblPassword.Size = new Size(227, 41);
             lblPassword.TabIndex = 5;
             lblPassword.Text = "PASSWORD ";
             lblPassword.Click += lblPassword_Click;
@@ -110,24 +106,22 @@
             // textBox1
             // 
             textBox1.BackColor = Color.Khaki;
-            textBox1.Location = new Point(568, 508);
-            textBox1.Margin = new Padding(2);
+            textBox1.Location = new Point(811, 811);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
             textBox1.PlaceholderText = "Enter your username";
-            textBox1.Size = new Size(326, 30);
+            textBox1.Size = new Size(464, 47);
             textBox1.TabIndex = 6;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
             textBox2.BackColor = Color.Khaki;
-            textBox2.Location = new Point(568, 552);
-            textBox2.Margin = new Padding(2);
+            textBox2.Location = new Point(811, 884);
             textBox2.Multiline = true;
             textBox2.Name = "textBox2";
             textBox2.PlaceholderText = "Enter your password";
-            textBox2.Size = new Size(326, 32);
+            textBox2.Size = new Size(464, 51);
             textBox2.TabIndex = 7;
             textBox2.UseSystemPasswordChar = true;
             // 
@@ -136,10 +130,9 @@
             btnEnter.BackColor = Color.Khaki;
             btnEnter.Font = new Font("Times New Roman", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnEnter.ForeColor = Color.DimGray;
-            btnEnter.Location = new Point(475, 622);
-            btnEnter.Margin = new Padding(2);
+            btnEnter.Location = new Point(679, 1001);
             btnEnter.Name = "btnEnter";
-            btnEnter.Size = new Size(106, 31);
+            btnEnter.Size = new Size(151, 52);
             btnEnter.TabIndex = 8;
             btnEnter.Text = "ENTER";
             btnEnter.UseVisualStyleBackColor = false;
@@ -150,21 +143,21 @@
             btnCancel.BackColor = Color.Khaki;
             btnCancel.Font = new Font("Times New Roman", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnCancel.ForeColor = Color.DimGray;
-            btnCancel.Location = new Point(657, 622);
-            btnCancel.Margin = new Padding(2);
+            btnCancel.Location = new Point(939, 1001);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(106, 31);
+            btnCancel.Size = new Size(151, 52);
             btnCancel.TabIndex = 9;
             btnCancel.Text = "CANCEL";
             btnCancel.UseVisualStyleBackColor = false;
+            btnCancel.Click += btnCancel_Click;
             // 
             // Login
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(1247, 637);
+            ClientSize = new Size(1774, 1116);
             Controls.Add(btnCancel);
             Controls.Add(btnEnter);
             Controls.Add(textBox2);
@@ -174,7 +167,7 @@
             Controls.Add(lblaim);
             Controls.Add(lblname);
             Controls.Add(lbllogin);
-            Margin = new Padding(2);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Login";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "LOGIN";
